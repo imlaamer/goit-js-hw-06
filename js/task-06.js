337b9d7fbc229665ed4ca6d14b33023cf48c8bf7
imlaamer <147347232+imlaamer@users.noreply.github.com>
2023-10-09 00:08:35 +0300

    Update task-06.js
    ==

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,7 @@ input.addEventListener('blur', checkValidationInput)
 function checkValidationInput (event) {
 const inpuValue = event.target.value.trim()
 
-if (inpuValue.length >= input.dataset.length ) {
+if (inpuValue.length == input.dataset.length ) {
     input.classList.remove('invalid');
     input.classList.add('valid');
    
