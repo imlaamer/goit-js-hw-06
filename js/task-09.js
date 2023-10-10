7a670036bfedd954ea955ba607f6986b7a148fdb
imlaamer <147347232+imlaamer@users.noreply.github.com>
2023-10-10 17:31:06 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,18 +2,16 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-
 }
 
 const buttonChangeColor = document.querySelector('.change-color')
 buttonChangeColor.addEventListener('click', changeColor)
 
 function changeColor() {
-  const widget = document.querySelector('.widget')
+  const body = document.querySelector('body')
   let randomColor = getRandomHexColor();
-  widget.style.backgroundColor = randomColor;
+  body.style.backgroundColor = randomColor;
   
   const colorText = document.querySelector('.color') 
   colorText.textContent = randomColor;
 }
-
