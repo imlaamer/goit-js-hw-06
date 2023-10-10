49bc9349761b6dafb912fa6bb567637c2f311122
imlaamer <147347232+imlaamer@users.noreply.github.com>
2023-10-10 17:29:02 +0300

    Update task-05.js
    Anonymous

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,17 @@
 const span = document.querySelector('#name-output')
 const input = document.querySelector('#name-input')
 
-input.addEventListener('input', changeCurrentName )
+input.addEventListener('input', changeCurrentName)
 
 function changeCurrentName(event) {
-    span.textContent = event.target.value.trim();
+    const currentValue = event.target.value.trim()
+    const spanText = span.textContent;
+
+    if(currentValue == '') {
+        // span.textContent = 'Anonymous'; 
+        span.textContent = spanText; //не впевнена як вірно. ідея була в тому,якщо наприклад зміниться розмітка в html не змінювати скрипт
+        return;
+    }
+    span.textContent = currentValue;
 }
 
