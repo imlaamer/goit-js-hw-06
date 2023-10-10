48e650fcd7c38cbc5e9d81f25aba57c6217a24df
imlaamer <147347232+imlaamer@users.noreply.github.com>
2023-10-10 17:42:26 +0300

    Update task-05.js
        span.textContent = 'Anonymous';  2

--- js/task-05.js
+++ js/task-05.js
@@ -5,11 +5,9 @@ input.addEventListener('input', changeCurrentName)
 
 function changeCurrentName(event) {
     const currentValue = event.target.value.trim()
-    const spanText = span.textContent;
 
     if(currentValue == '') {
        span.textContent = 'Anonymous'; 
-        //span.textContent = spanText; //не впевнена як вірно. ідея була в тому,якщо наприклад зміниться розмітка в html не змінювати скрипт
         return;
     }
     span.textContent = currentValue;
