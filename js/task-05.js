7d07f663b2a20d4bbb80238b91f9722f8207a4b7
imlaamer <147347232+imlaamer@users.noreply.github.com>
2023-10-10 17:39:38 +0300

    Update task-05.js
           span.textContent = 'Anonymous';

--- js/task-05.js
+++ js/task-05.js
@@ -8,8 +8,8 @@ function changeCurrentName(event) {
     const spanText = span.textContent;
 
     if(currentValue == '') {
-        // span.textContent = 'Anonymous'; 
-        span.textContent = spanText; //не впевнена як вірно. ідея була в тому,якщо наприклад зміниться розмітка в html не змінювати скрипт
+       span.textContent = 'Anonymous'; 
+        //span.textContent = spanText; //не впевнена як вірно. ідея була в тому,якщо наприклад зміниться розмітка в html не змінювати скрипт
         return;
     }
     span.textContent = currentValue;
